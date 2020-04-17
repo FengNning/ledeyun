a4ed9e8d9b5cdd847db1fbc5c1a8f79b5b4db22e
穆峰 <36254159+FengNning@users.noreply.github.com>
2020-04-18 04:37:24 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+mkdir package/base-files/files/config
+echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
+echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
 #=================================================
 # Description: DIY script
 # Lisence: MIT
@@ -7,7 +10,3 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-#=================================================
-mkdir package/base-files/files/config
-echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
-echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
