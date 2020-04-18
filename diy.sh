b9e201b35a1c8ef5586a6c683306817c5c03ff95
穆峰 <36254159+FengNning@users.noreply.github.com>
2020-04-18 09:13:01 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-mkdir package/base-files/files/config
-echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
-echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+#mkdir package/base-files/files/config
+#echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
+#echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
 #=================================================
 # Description: DIY script
 # Lisence: MIT
